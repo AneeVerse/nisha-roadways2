faa0134c682f78d4ec42ebfb9057fb27cf5ff0a9
mayur5689 <aneeverse@example.com>
2025-10-09 23:01:46 +0530

    refactor(page): comment out TrustedByBusiness component
    Temporarily removing the component while redesigning the business section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <CompanyLogos />
       <Feature />
       <Solutions />
-      <TrustedByBusiness />
+      {/* <TrustedByBusiness /> */}
       <SocialMosaic />
       <ReviewVideo />
           <Testimonials />
